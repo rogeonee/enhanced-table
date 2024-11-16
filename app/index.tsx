e15f35105e82964f6aaed60ee986e79d32aebc21
Egor <egormaxb@gmail.com>
2024-11-15 22:42:19 -0800

    Setup with tw and rnr

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { View } from 'react-native';
+import { Button } from '~/components/ui/button';
+import { Text } from '~/components/ui/text';
+
+const MenuScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center gap-8">
+      <Text className="text-3xl font-bold my-10">Enhanced Table</Text>
+      <Button size="lg">
+        <Text className="text-lg font-bold">Playground</Text>
+      </Button>
+      <Button size="lg">
+        <Text className="text-lg font-bold">Start Study</Text>
+      </Button>
+      <Button size="lg">
+        <Text className="text-lg font-bold">Options</Text>
+      </Button>
+    </View>
+  );
+};
+
+export default MenuScreen;
